a9c4fc831ef9b93003a89885b9500b81a568ac07
Matthew Phelps <mphelps3@nd.edu>
2017-03-01 15:41:16 -0500

    Add files via upload

--- PlottingDungeonMapNotes.m
+++ PlottingDungeonMapNotes.m
@@ -2,7 +2,11 @@
 
 % Notes!
 
-% Starting point should probably be at least three away from wall of map
+% Starting point should probably be in middle ninth of map
+% This means if width is positions 2-13 and height is positions 2-10,
+%     the start room should be in the middle third of both of these ranges:
+%     range for width  =  6-9 
+%     range for height =  5-8
 
 %% Naming convention
 
@@ -18,7 +22,5 @@
 % Normal room being generated = 1
 % Exit = 4
 % Treasure = 5
+%     has only one entrance, uses function generateRest_treasureRoom#
 % Shop = 7
-
-
-
