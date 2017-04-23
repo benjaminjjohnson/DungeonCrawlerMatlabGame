79d479b68acd1c78a002556d3403e36316a18836
Matthew Phelps <mphelps3@nd.edu>
2017-04-23 11:11:45 -0400

    Weapon Names

---
+++ WeaponNames.m
@@ -0,0 +1,25 @@
+% ~~~~~~~~~~~~~
+% Example Weapon Names
+% ~~~~~~~~~~~~~
+
+
+% Forbidden Dagger
+% Trahka's Dagger
+% Winter Dagger
+% Dagger of Thorns
+% Pahrki's Dagger
+
+% Forbidden Sword
+% Guardian Sword
+% Sword of Dreams
+% Ghasha's Sword
+% Shurze's Sword
+% Royal Gold Sword
+% Royal Red Sword
+
+% Forbidden Axe
+% Carpenter's Axe
+% Farhma's Axe
+% Springtime Axe
+% Fihnshe's Axe
+% Pluw's Axe
